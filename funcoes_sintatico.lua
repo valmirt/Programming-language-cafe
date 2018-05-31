5b18668b0c4d4ecbb4119845b7025ad9820970f5
Valmir <valmirjr_20@hotmail.com>
2018-05-31 19:48:46 -0300

    Finalizando a tabela shift/reduce

--- funcoes_sintatico.lua
+++ funcoes_sintatico.lua
@@ -146,7 +146,7 @@ function tabela_sintatica_sr ()
 	--Estado 13
 	tabela_shift_reduce[13][12] = { ['operacao'] = 'Shift', ['estado'] = 28,} -- terminal rcb
 	
-	--Estado s14
+	--Estado 14
 	tabela_shift_reduce[14][8] = { ['operacao'] = 'Shift', ['estado'] = 11,} --terminal leia
 	tabela_shift_reduce[14][9] = { ['operacao'] = 'Shift', ['estado'] = 13,} --terminal id
 	tabela_shift_reduce[14][10] = { ['operacao'] = 'Shift', ['estado'] = 12,} --terminal escreva
@@ -175,8 +175,230 @@ function tabela_sintatica_sr ()
 	tabela_shift_reduce[17][26] = { ['operacao'] = nil, ['estado'] = 17,} --nao-terminal D
 	
 	--Estado 18
+	tabela_shift_reduce[18][4] = { ['operacao'] = 'Shift', [2] = 35,} -- terminal ;
 	
 	--Estado 19
+	tabela_shift_reduce[19][5] = { ['operacao'] = 'Shift', [2] = 37,} -- terminal varfim
+	tabela_shift_reduce[19][6] = { ['operacao'] = 'Shift', [2] = 38,} -- terminal varfim
+	tabela_shift_reduce[19][7] = { ['operacao'] = 'Shift', [2] = 39,} -- terminal varfim
+	tabela_shift_reduce[19][27] = { ['operacao'] = nil, ['estado'] = 36,} --nao-terminal TIPO
 	
-	--Estado 20
+	--Estado 20 --Estado de redução--
+	tabela_shift_reduce[20][21] = { ['operacao'] = 'Reduce', ['estado'] = 10,} --termial $
+	
+	--Estado 21 --Estado de redução--
+	tabela_shift_reduce[21][21] = { ['operacao'] = 'Reduce', ['estado'] = 16,} --termial $
+	
+	--Estado 22 --Estado de redução--
+	tabela_shift_reduce[22][21] = { ['operacao'] = 'Reduce', ['estado'] = 22,} --termial $
+	
+	--Estado 23
+	tabela_shift_reduce[23][4] = { ['operacao'] = 'Shift', [2] = 40,} -- terminal ;
+	
+	--Estado 24
+	tabela_shift_reduce[24][4] = { ['operacao'] = 'Shift', [2] = 59,} -- terminal ;
+	
+	--Estado 25 --Estado de redução
+	tabela_shift_reduce[25][4] = { ['operacao'] = 'Reduce', ['estado'] = 13,} --termial ;
+	
+	--Estado 26 --Estado de redução
+	tabela_shift_reduce[26][4] = { ['operacao'] = 'Reduce', ['estado'] = 14,} --termial ;
+	
+	--Estado 27 --Estado de redução
+	tabela_shift_reduce[27][4] = { ['operacao'] = 'Reduce', ['estado'] = 15,} --termial ;
+	
+	--Estado 28
+	tabela_shift_reduce[28][9] = { ['operacao'] = 'Shift', [2] = 43,} -- terminal id
+	tabela_shift_reduce[28][11] = { ['operacao'] = 'Shift', [2] = 44,} -- terminal num
+	tabela_shift_reduce[28][31] = { ['operacao'] = nil, ['estado'] = 41,} --nao-terminal LD
+	tabela_shift_reduce[28][32] = { ['operacao'] = nil, ['estado'] = 42,} --nao-terminal OPRD
+	
+	--Estado 29 --Estado de redução
+	tabela_shift_reduce[29][8] = { ['operacao'] = 'Reduce', ['estado'] = 23,} --termial leia
+	tabela_shift_reduce[29][9] = { ['operacao'] = 'Reduce', ['estado'] = 23,} --termial id
+	tabela_shift_reduce[29][10] = { ['operacao'] = 'Reduce', ['estado'] = 23,} --termial escreva	
+	tabela_shift_reduce[29][14] = { ['operacao'] = 'Reduce', ['estado'] = 23,} --termial se
+	tabela_shift_reduce[29][19] = { ['operacao'] = 'Reduce', ['estado'] = 23,} --termial fimse
+	tabela_shift_reduce[29][20] = { ['operacao'] = 'Reduce', ['estado'] = 23,} --termial fim
+	
+	--Estado 30
+	tabela_shift_reduce[30][8] = { ['operacao'] = 'Shift', ['estado'] = 11,} --terminal leia
+	tabela_shift_reduce[30][9] = { ['operacao'] = 'Shift', ['estado'] = 13,} --terminal id
+	tabela_shift_reduce[30][10] = { ['operacao'] = 'Shift', ['estado'] = 12,} --terminal escreva
+	tabela_shift_reduce[30][14] = { ['operacao'] = 'Shift', ['estado'] = 15,} -- terminal se
+	tabela_shift_reduce[30][19] = { ['operacao'] = 'Shift', ['estado'] = 58,} -- terminal fimse
+	tabela_shift_reduce[30][28] = { ['operacao'] = nil, ['estado'] = 30,} --nao-terminal ES
+	tabela_shift_reduce[30][30] = { ['operacao'] = nil, ['estado'] = 31,} --nao-terminal CMD
+	tabela_shift_reduce[30][33] = { ['operacao'] = nil, ['estado'] = 32,} --nao-terminal COND
+	tabela_shift_reduce[30][34] = { ['operacao'] = nil, ['estado'] = 14,} --nao-terminal CABEÇALHO
+	tabela_shift_reduce[30][36] = { ['operacao'] = nil, ['estado'] = 45,} --nao-terminal CORPO
+	
+	--Estado 31
+	tabela_shift_reduce[31][8] = { ['operacao'] = 'Shift', ['estado'] = 11,} --terminal leia
+	tabela_shift_reduce[31][9] = { ['operacao'] = 'Shift', ['estado'] = 13,} --terminal id
+	tabela_shift_reduce[31][10] = { ['operacao'] = 'Shift', ['estado'] = 12,} --terminal escreva
+	tabela_shift_reduce[31][14] = { ['operacao'] = 'Shift', ['estado'] = 15,} -- terminal se
+	tabela_shift_reduce[31][19] = { ['operacao'] = 'Shift', ['estado'] = 58,} -- terminal fimse
+	tabela_shift_reduce[31][28] = { ['operacao'] = nil, ['estado'] = 30,} --nao-terminal ES
+	tabela_shift_reduce[31][30] = { ['operacao'] = nil, ['estado'] = 31,} --nao-terminal CMD
+	tabela_shift_reduce[31][33] = { ['operacao'] = nil, ['estado'] = 32,} --nao-terminal COND
+	tabela_shift_reduce[31][34] = { ['operacao'] = nil, ['estado'] = 14,} --nao-terminal CABEÇALHO
+	tabela_shift_reduce[31][36] = { ['operacao'] = nil, ['estado'] = 46,} --nao-terminal CORPO
+	
+	--Estado 32
+	tabela_shift_reduce[32][8] = { ['operacao'] = 'Shift', ['estado'] = 11,} --terminal leia
+	tabela_shift_reduce[32][9] = { ['operacao'] = 'Shift', ['estado'] = 13,} --terminal id
+	tabela_shift_reduce[32][10] = { ['operacao'] = 'Shift', ['estado'] = 12,} --terminal escreva
+	tabela_shift_reduce[32][14] = { ['operacao'] = 'Shift', ['estado'] = 15,} -- terminal se
+	tabela_shift_reduce[32][19] = { ['operacao'] = 'Shift', ['estado'] = 58,} -- terminal fimse
+	tabela_shift_reduce[32][28] = { ['operacao'] = nil, ['estado'] = 30,} --nao-terminal ES
+	tabela_shift_reduce[32][30] = { ['operacao'] = nil, ['estado'] = 31,} --nao-terminal CMD
+	tabela_shift_reduce[32][33] = { ['operacao'] = nil, ['estado'] = 32,} --nao-terminal COND
+	tabela_shift_reduce[32][34] = { ['operacao'] = nil, ['estado'] = 14,} --nao-terminal CABEÇALHO
+	tabela_shift_reduce[32][36] = { ['operacao'] = nil, ['estado'] = 47,} --nao-terminal CORPO
+	
+	--Estado 33
+	tabela_shift_reduce[33][9] = { ['operacao'] = 'Shift', [2] = 43,} -- terminal id
+	tabela_shift_reduce[33][11] = { ['operacao'] = 'Shift', [2] = 44,} -- terminal num
+	tabela_shift_reduce[33][32] = { ['operacao'] = nil, ['estado'] = 49,} --nao-terminal OPRD
+	tabela_shift_reduce[33][35] = { ['operacao'] = nil, ['estado'] = 48,} --nao-terminal EXP_R
+	
+	--Estado 34 --Estado de redução
+	tabela_shift_reduce[34][8] = { ['operacao'] = 'Reduce', ['estado'] = 4,} --termial leia
+	tabela_shift_reduce[34][9] = { ['operacao'] = 'Reduce', ['estado'] = 4,} --termial id
+	tabela_shift_reduce[34][10] = { ['operacao'] = 'Reduce', ['estado'] = 4,} --termial escreva
+	tabela_shift_reduce[34][14] = { ['operacao'] = 'Reduce', ['estado'] = 4,} --termial se
+	tabela_shift_reduce[34][20] = { ['operacao'] = 'Reduce', ['estado'] = 4,} --termial fim
+	
+	--Estado 35 --Estado de redução
+	tabela_shift_reduce[35][8] = { ['operacao'] = 'Reduce', ['estado'] = 5,} --termial leia
+	tabela_shift_reduce[35][9] = { ['operacao'] = 'Reduce', ['estado'] = 5,} --termial id
+	tabela_shift_reduce[35][10] = { ['operacao'] = 'Reduce', ['estado'] = 5,} --termial escreva
+	tabela_shift_reduce[35][14] = { ['operacao'] = 'Reduce', ['estado'] = 5,} --termial se
+	tabela_shift_reduce[35][20] = { ['operacao'] = 'Reduce', ['estado'] = 5,} --termial fim
+	
+	--Estado 36
+	tabela_shift_reduce[36][4] = { ['operacao'] = 'Shift', ['estado'] = 50,} --termial ;
+	
+	--Estado 37 --Estado de redução
+	tabela_shift_reduce[37][4] = { ['operacao'] = 'Reduce', ['estado'] = 7,} --termial ;
+	
+	--Estado 38 --Estado de redução
+	tabela_shift_reduce[38][4] = { ['operacao'] = 'Reduce', ['estado'] = 8,} --termial ;
+	
+	--Estado 39 --Estado de redução
+	tabela_shift_reduce[39][4] = { ['operacao'] = 'Reduce', ['estado'] = 9,} --termial ;
+	
+	--Estado 40 --Estado de redução
+	tabela_shift_reduce[40][8] = { ['operacao'] = 'Reduce', ['estado'] = 11,} --termial leia
+	tabela_shift_reduce[40][9] = { ['operacao'] = 'Reduce', ['estado'] = 11,} --termial id
+	tabela_shift_reduce[40][10] = { ['operacao'] = 'Reduce', ['estado'] = 11,} --termial escreva	
+	tabela_shift_reduce[40][14] = { ['operacao'] = 'Reduce', ['estado'] = 11,} --termial se
+	tabela_shift_reduce[40][19] = { ['operacao'] = 'Reduce', ['estado'] = 11,} --termial fimse
+	tabela_shift_reduce[40][20] = { ['operacao'] = 'Reduce', ['estado'] = 11,} --termial fim
+	
+	--Estado 41
+	tabela_shift_reduce[41][4] = { ['operacao'] = 'Shift', ['estado'] = 51,} --termial ;
+	
+	--Estado 42 --Estado de redução
+	tabela_shift_reduce[42][4] = { ['operacao'] = 'Reduce', ['estado'] = 19,} --termial ;
+	tabela_shift_reduce[42][13] = { ['operacao'] = 'Shift', ['estado'] = 52,} --termial opm
+	
+	--Estado 43 --Estado de redução
+	tabela_shift_reduce[43][4] = { ['operacao'] = 'Reduce', ['estado'] = 20,} --termial ;
+	tabela_shift_reduce[43][13] = { ['operacao'] = 'Reduce', ['estado'] = 20,} --termial opm
+	tabela_shift_reduce[43][16] = { ['operacao'] = 'Reduce', ['estado'] = 20,} --termial )
+	tabela_shift_reduce[43][18] = { ['operacao'] = 'Reduce', ['estado'] = 20,} --termial opr
+	
+	--Estado 44 --Estado de redução
+	tabela_shift_reduce[44][4] = { ['operacao'] = 'Reduce', ['estado'] = 21,} --termial ;
+	tabela_shift_reduce[44][13] = { ['operacao'] = 'Reduce', ['estado'] = 21,} --termial opm
+	tabela_shift_reduce[44][16] = { ['operacao'] = 'Reduce', ['estado'] = 21,} --termial )
+	tabela_shift_reduce[44][18] = { ['operacao'] = 'Reduce', ['estado'] = 21,} --termial opr
+	
+	--Estado 45 --Estado de redução
+	tabela_shift_reduce[45][8] = { ['operacao'] = 'Reduce', ['estado'] = 26,} --termial leia
+	tabela_shift_reduce[45][9] = { ['operacao'] = 'Reduce', ['estado'] = 26,} --termial id
+	tabela_shift_reduce[45][10] = { ['operacao'] = 'Reduce', ['estado'] = 26,} --termial escreva	
+	tabela_shift_reduce[45][14] = { ['operacao'] = 'Reduce', ['estado'] = 26,} --termial se
+	tabela_shift_reduce[45][19] = { ['operacao'] = 'Reduce', ['estado'] = 26,} --termial fimse
+	tabela_shift_reduce[45][20] = { ['operacao'] = 'Reduce', ['estado'] = 26,} --termial fim
+	
+	--Estado 46 --Estado de redução
+	tabela_shift_reduce[46][8] = { ['operacao'] = 'Reduce', ['estado'] = 27,} --termial leia
+	tabela_shift_reduce[46][9] = { ['operacao'] = 'Reduce', ['estado'] = 27,} --termial id
+	tabela_shift_reduce[46][10] = { ['operacao'] = 'Reduce', ['estado'] = 27,} --termial escreva	
+	tabela_shift_reduce[46][14] = { ['operacao'] = 'Reduce', ['estado'] = 27,} --termial se
+	tabela_shift_reduce[46][19] = { ['operacao'] = 'Reduce', ['estado'] = 27,} --termial fimse
+	tabela_shift_reduce[46][20] = { ['operacao'] = 'Reduce', ['estado'] = 27,} --termial fim
+	
+	--Estado 47 --Estado de redução
+	tabela_shift_reduce[47][8] = { ['operacao'] = 'Reduce', ['estado'] = 28,} --termial leia
+	tabela_shift_reduce[47][9] = { ['operacao'] = 'Reduce', ['estado'] = 28,} --termial id
+	tabela_shift_reduce[47][10] = { ['operacao'] = 'Reduce', ['estado'] = 28,} --termial escreva	
+	tabela_shift_reduce[47][14] = { ['operacao'] = 'Reduce', ['estado'] = 28,} --termial se
+	tabela_shift_reduce[47][19] = { ['operacao'] = 'Reduce', ['estado'] = 28,} --termial fimse
+	tabela_shift_reduce[47][20] = { ['operacao'] = 'Reduce', ['estado'] = 28,} --termial fim
+	
+	--Estado 48
+	tabela_shift_reduce[48][16] = { ['operacao'] = 'Shift', ['estado'] = 53,} --termial )
+	
+	--Estado 49
+	tabela_shift_reduce[49][18] = { ['operacao'] = 'Shift', ['estado'] = 54,} --termial opr
+	
+	--Estado 50 --Estado de redução
+	tabela_shift_reduce[50][3] = { ['operacao'] = 'Reduce', ['estado'] = 6,} --termial varfim
+	tabela_shift_reduce[50][9] = { ['operacao'] = 'Reduce', ['estado'] = 6,} --termial id
+	
+	--Estado 51 --Estado de redução
+	tabela_shift_reduce[51][8] = { ['operacao'] = 'Reduce', ['estado'] = 17,} --termial leia
+	tabela_shift_reduce[51][9] = { ['operacao'] = 'Reduce', ['estado'] = 17,} --termial id
+	tabela_shift_reduce[51][10] = { ['operacao'] = 'Reduce', ['estado'] = 17,} --termial escreva	
+	tabela_shift_reduce[51][14] = { ['operacao'] = 'Reduce', ['estado'] = 17,} --termial se
+	tabela_shift_reduce[51][19] = { ['operacao'] = 'Reduce', ['estado'] = 17,} --termial fimse
+	tabela_shift_reduce[51][20] = { ['operacao'] = 'Reduce', ['estado'] = 17,} --termial fim
+	
+	--Estado 52
+	tabela_shift_reduce[52][9] = { ['operacao'] = 'Shift', [2] = 43,} -- terminal id
+	tabela_shift_reduce[52][11] = { ['operacao'] = 'Shift', [2] = 44,} -- terminal num
+	tabela_shift_reduce[52][32] = { ['operacao'] = nil, ['estado'] = 55,} --nao-terminal OPRD
+	
+	--Estado 53
+	tabela_shift_reduce[53][17] = { ['operacao'] = 'Shift', [2] = 56,} -- terminal entao
+	
+	--Estado 54
+	tabela_shift_reduce[54][9] = { ['operacao'] = 'Shift', [2] = 43,} -- terminal id
+	tabela_shift_reduce[54][11] = { ['operacao'] = 'Shift', [2] = 44,} -- terminal num
+	tabela_shift_reduce[54][32] = { ['operacao'] = nil, ['estado'] = 57,} --nao-terminal OPRD
+	
+	--Estado 55 --Estado de redução
+	tabela_shift_reduce[55][4] = { ['operacao'] = 'Reduce', ['estado'] = 18,} --termial ;
+	
+	--Estado 56 --Estado de redução
+	tabela_shift_reduce[56][8] = { ['operacao'] = 'Reduce', ['estado'] = 24,} --termial leia
+	tabela_shift_reduce[56][9] = { ['operacao'] = 'Reduce', ['estado'] = 24,} --termial id
+	tabela_shift_reduce[56][10] = { ['operacao'] = 'Reduce', ['estado'] = 24,} --termial escreva	
+	tabela_shift_reduce[56][14] = { ['operacao'] = 'Reduce', ['estado'] = 24,} --termial se
+	tabela_shift_reduce[56][19] = { ['operacao'] = 'Reduce', ['estado'] = 24,} --termial fimse
+	
+	--Estado 57 --Estado de redução
+	tabela_shift_reduce[57][16] = { ['operacao'] = 'Reduce', ['estado'] = 25,} --termial )
+	
+	--Estado 58 --Estado de redução
+	tabela_shift_reduce[58][8] = { ['operacao'] = 'Reduce', ['estado'] = 29,} --termial leia
+	tabela_shift_reduce[58][9] = { ['operacao'] = 'Reduce', ['estado'] = 29,} --termial id
+	tabela_shift_reduce[58][10] = { ['operacao'] = 'Reduce', ['estado'] = 29,} --termial escreva	
+	tabela_shift_reduce[58][14] = { ['operacao'] = 'Reduce', ['estado'] = 29,} --termial se
+	tabela_shift_reduce[58][19] = { ['operacao'] = 'Reduce', ['estado'] = 29,} --termial fimse
+	tabela_shift_reduce[58][20] = { ['operacao'] = 'Reduce', ['estado'] = 29,} --termial fim
+	
+	--Estado 59 --Estado de redução
+	tabela_shift_reduce[59][8] = { ['operacao'] = 'Reduce', ['estado'] = 12,} --termial leia
+	tabela_shift_reduce[59][9] = { ['operacao'] = 'Reduce', ['estado'] = 12,} --termial id
+	tabela_shift_reduce[59][10] = { ['operacao'] = 'Reduce', ['estado'] = 12,} --termial escreva	
+	tabela_shift_reduce[59][14] = { ['operacao'] = 'Reduce', ['estado'] = 12,} --termial se
+	tabela_shift_reduce[59][19] = { ['operacao'] = 'Reduce', ['estado'] = 12,} --termial fimse
+	tabela_shift_reduce[59][20] = { ['operacao'] = 'Reduce', ['estado'] = 12,} --termial fim
+	
+	return tabela_shift_reduce
 end
