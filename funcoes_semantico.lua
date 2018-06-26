08f80d93eb48f4cb42743680ec0c0442beeddd18
Valmir Torres <valmirjr_20@hotmail.com>
2018-06-26 00:48:54 -0300

    Implementando as regras semânticas

--- funcoes_semantico.lua
+++ funcoes_semantico.lua
@@ -1,9 +1,9 @@
 --[[Valmir Torres de Jesus Junior
 	Compiladores 24-06-2018
-		
+
 	Compilador feito em lua que dado um arquivo em Mgol é convertido
 	para linguagem C.
-	
+
 	-Funções necessárias e o analisador semântico-
 ]]
 
@@ -19,48 +19,38 @@ function analisador_semantico (file, regra, terminal)
 		--Imprime tres linhas em branco no arquivo
 		file = file..'\n\n\n'
 	elseif regra == 6 then
-		--Procura o terminal na tabela de simbolos e atribui o tipo dele
-		for i = 1, #tabela_simbolos do
-			if tabela_simbolos[i].lexema == terminal.lexema then
-				tabela_simbolos[i].tipo = nao_terminais.TIPO.tipo
-				terminal.tipo = tabela_simbolos[i].tipo
-				break
-			end
-		end
-		file = file..nao_terminais.TIPO.tipo..' '..terminal.lexema..';\n'
 		print('Define o tipo do id recido pelo não terminal TIPO')
 		print('Escreve a sentença no arquivo')
+		file = file..nao_terminais.TIPO.tipo..' '..terminal.lexema..';\n'
 	elseif regra == 7 then
-		nao_terminais.TIPO.tipo = tabela_simbolos[11].tipo
 		print('Define o tipo do não terminal TIPO como int')
+		nao_terminais.TIPO.tipo = tabela_simbolos[11].tipo
 	elseif regra == 8 then
-		nao_terminais.TIPO.tipo = tabela_simbolos[13].tipo
 		print('Define o tipo do não terminal TIPO como real')
+		nao_terminais.TIPO.tipo = tabela_simbolos[13].tipo
 	elseif regra == 9 then
-		nao_terminais.TIPO.tipo = tabela_simbolos[12].tipo
 		print('Define o tipo do não terminal TIPO como lit')
+		nao_terminais.TIPO.tipo = tabela_simbolos[12].tipo
 	elseif regra == 11 then
 		print('Verfica se o campo tipo do id está preenchido')
 		print('Escreve a sentença no arquivo')
 		--Procura o terminal na tabela de simbolos e escreve a sentença no arquivo
-		for i = 1, #tabela_simbolos do
-			if tabela_simbolos[i].token == terminal.token then
-				if tabela_simbolos[i].tipo == 'int' then
-					file = file..'scanf("%s", '..tabela_simbolos[i].lexema..');\n'
-				elseif tabela_simbolos[i].tipo == 'real' then
-					file = file..'scanf("%d", &'..tabela_simbolos[i].lexema..');\n'
-				elseif tabela_simbolos[i].tipo == 'lit' then
-					file = file..'scanf("%1f", &'..tabela_simbolos[i].lexema..');\n'
-				else 
-					print('Erro!! Variável '..tabela_simbolos[i].lexema..' não declarada...')
-					erro = true
-					erro_semantico = true
-				end
-				break
-			end
+		if terminal.tipo == 'int' then
+			file = file..'scanf("%s", '..termial.lexema..');\n'
+		elseif terminal.tipo == 'real' then
+			file = file..'scanf("%d", &'..termial.lexema..');\n'
+		elseif terminal.tipo == 'lit' then
+			file = file..'scanf("%1f", &'..termial.lexema..');\n'
+		else
+			print('Erro!! Variável '..termial.lexema..' não declarada...')
+			erro = true
+			erro_semantico = true
 		end
 	elseif regra == 12 then
+		print('Gerar código para o comando escreva')
+		file = file..'printf("'..nao_terminais.ARG.lexema..'");\n'
 	elseif regra == 13 then
+		
 	elseif regra == 14 then
 	elseif regra == 15 then
 	elseif regra == 16 then
@@ -73,7 +63,7 @@ function analisador_semantico (file, regra, terminal)
 	elseif regra == 24 then
 	elseif regra == 25 then
 	end
-	
+
 	return file
 end
 
@@ -145,7 +135,7 @@ function palavras_reservadas ()
 			['tipo'] = 'real',
 		},
 	}
-	
+
 	return tabela_simbolos
 end
 
@@ -233,7 +223,6 @@ function atributos_nterminais()
 			['tipo'] = nil,
 		},
 	}
-	
+
 	return tabela_nterminais
 end
-
